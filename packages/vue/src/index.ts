d23a33286dc79de554d3413dbdc6ebdc5ca1339c
amk-dev <akash.k.mohan98@gmail.com>
2022-08-10 04:32:12 +0530

    chore: add component exports to index.ts

--- packages/vue/src/index.ts
+++ packages/vue/src/index.ts
@@ -1,1 +1,7 @@
-export {};
+import FullNoun from "./components/FullNoun.vue";
+import NounBody from "./components/NounBody.vue";
+import NounHead from "./components/NounHead.vue";
+import NounAccessory from "./components/NounAccessory.vue";
+import NounGlasses from "./components/NounGlasses.vue";
+
+export { FullNoun, NounBody, NounHead, NounAccessory, NounGlasses };
