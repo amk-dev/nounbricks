44ea00d8373fc46f8de3369daa1f73888e98aae1
amk-dev <akash.k.mohan98@gmail.com>
2022-10-21 03:06:42 +0530

    feat: add palettes, bgColors and imports for /heads, /bodies etc

--- packages/traits-nouns/src/index.ts
+++ packages/traits-nouns/src/index.ts
@@ -2,3 +2,5 @@ export * from "./heads";
 export * from "./glasses";
 export * from "./bodies";
 export * from "./accessories";
+export { default as palette } from "./palette";
+export { default as bgColors } from "./bgColors";
